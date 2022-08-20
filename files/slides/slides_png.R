4ea513ef4279c06209f7f026307d8d4044eb6a61
Ryan Safner <ryansafner@gmail.com>
2022-08-20 08:21:32 -0400

    Render, slides may be wrong

--- files/slides/slides_png.R
+++
@@ -1,2 +0,0 @@
-renderthis::to_png(from = "files/slides/1.1-slides.html",
-                   slides = 1)
---
+++ slides/slides_pdf_png.R
@@ -0,0 +1,4 @@
+renderthis::to_png(from = "slides/1.1-slides.html",
+                   slides = 1)
+
+renderthis::to_pdf(from = "slides/1.1-slides.html")
