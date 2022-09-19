9f48a7cfb4e1f2c2ed101f4e23674d836c513790
Ryan Safner <ryansafner@gmail.com>
2022-09-18 21:14:50 -0400

    2.2

--- _site/slides/slides_pdf_png.R
+++ _site/slides/slides_pdf_png.R
@@ -1,4 +1,4 @@
-renderthis::to_png(from = "slides/2.1-slides.html",
+renderthis::to_png(from = "slides/2.2-slides.html",
                    slides = 1)
 
-renderthis::to_pdf(from = "slides/2.1-slides.html")
+renderthis::to_pdf(from = "slides/2.2-slides.html")
