1b40d0585e67db693022f2f871e01ed03c101cdc
Ryan Safner <ryansafner@gmail.com>
2022-10-03 11:08:25 -0400

    Update 2.6 name, 2.6 slides, pdf

--- _site/slides/slides_pdf_png.R
+++ _site/slides/slides_pdf_png.R
@@ -1,4 +1,4 @@
-renderthis::to_png(from = "slides/2.5-slides.html",
+renderthis::to_png(from = "slides/2.6-slides.html",
                    slides = 1)
 
-renderthis::to_pdf(from = "slides/2.5-slides.html")
+renderthis::to_pdf(from = "slides/2.6-slides.html")
