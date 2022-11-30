e305acb97148c764ee6c4ea6f7bcda85ed8f4cfe
Ryan Safner <ryansafner@gmail.com>
2022-11-30 13:24:13 -0500

    Fix slides 5.3

--- _site/slides/slides_pdf_png.R
+++ _site/slides/slides_pdf_png.R
@@ -1,4 +1,4 @@
-renderthis::to_png(from = "slides/5.2-slides.html",
+renderthis::to_png(from = "slides/5.3-slides.html",
                    slides = 1)
 
-renderthis::to_pdf(from = "slides/5.2-slides.html")
+renderthis::to_pdf(from = "slides/5.3-slides.html")
